9a5442ad5118acbfb1c1d2d55b96cdb72b171a71
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2024-03-05 01:36:35 +0600

    1. Create Context folder. 2. Create CatalogApi db context class. 3. Setup connection string and database name in appsettings.json file.

---
+++ MicroserviceDemo/MicroserviceDemo.CatalogApi/Context/CatalogApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MongoRepo.Context;
+
+namespace MicroserviceDemo.CatalogApi.Context
+{
+    public class CatalogApiDbContext : ApplicationDbContext
+    {
+        // Get appsettings.json file access
+        private static IConfiguration configuration = new ConfigurationBuilder()
+                                                     .SetBasePath(Directory.GetCurrentDirectory())
+                                                     .AddJsonFile("appsettings.json", true, true)
+                                                     .Build();
+
+        // Get connection string and database name using configuration
+        private static string connectionString = configuration.GetConnectionString("CatalogApi");
+        private static string databaseName = configuration.GetValue<string>("DatabaseName");
+
+        public CatalogApiDbContext() : base(connectionString, databaseName)
+        {
+            
+        }
+    }
+}

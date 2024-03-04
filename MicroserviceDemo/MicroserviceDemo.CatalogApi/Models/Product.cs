d444e124be0859ce219efd976eb5ac13a92f46ef
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2024-03-05 00:41:25 +0600

    1. Install MongoRepo library. 2. Create product model.

---
+++ MicroserviceDemo/MicroserviceDemo.CatalogApi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MicroserviceDemo.CatalogApi.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageFile { get; set; }
+    }
+}

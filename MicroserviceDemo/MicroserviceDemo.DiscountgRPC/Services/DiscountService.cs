b44e67815aada4e5725ccfe27ef5cb8c672b7439
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2024-06-02 21:52:00 +0600

    Developing CRUD functionality in the DiscountService for the Discount.gRPC

--- MicroserviceDemo/MicroserviceDemo.DiscountgRPC/Services/DiscountService.cs
+++ MicroserviceDemo/MicroserviceDemo.DiscountgRPC/Services/DiscountService.cs
@@ -27,10 +27,54 @@ namespace MicroserviceDemo.DiscountgRPC.Services
 
             var mapExistProductDiscount = _mapper.Map<Coupon>(existProductDiscount);
 
-            //Set log
+            // Set log
             _logger.LogInformation($"Discount retrive for Product Name: {mapExistProductDiscount.ProductName}, Amount: {mapExistProductDiscount.Amount}");
 
             return mapExistProductDiscount;
+        }
+
+        public override async Task<Result> CreateDiscount(Coupon request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Models.Coupon>(request);
+            var isCouponSaved = await _couponRepository.CreateDiscount(coupon);
+
+            if (isCouponSaved)
+            {
+                _logger.LogInformation($"Discount is successfully created. Product name {coupon.ProductName}");
+                return new Result { Success = true };
+            }
+
+            _logger.LogInformation($"Discount is not created. Please, try again.");
+            return new Result { Success = false };
+        }
+
+        public override async Task<Result> UpdateDiscount(Coupon request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Models.Coupon>(request);
+            var isCouponUpdated = await _couponRepository.UpdateDiscount(coupon);
+
+            if (isCouponUpdated)
+            {
+                _logger.LogInformation($"Discount is updated. Product name {coupon.ProductName}");
+                return new Result { Success = true };
+            }
+
+            _logger.LogInformation($"Discount is not updated. Please, try again.");
+            return new Result { Success = false };
+        }
+
+        public override async Task<Result> DeleteDiscount(ProductId request, ServerCallContext context)
+        {
+            var isCoupnDeleted = await _couponRepository.DeleteDiscount(request.ProductId_);
+
+            if (isCoupnDeleted)
+            {
+                _logger.LogInformation($"Discount is deleted.");
+                return new Result { Success = true };
+            }
+
+            _logger.LogInformation($"Discount is not deleted. Please, try again.");
+            return new Result { Success = false };
         }
     }
 }
